69dd666a0ccc17aa7e06973403348dd47f1648c7
pontat <ponta.t.dev@gmail.com>
2021-09-21 00:31:15 +0900

    feat:task detail

--- components/Task.js
+++ components/Task.js
@@ -6,7 +6,9 @@ export default function Task({ task, taskDeleted }) {
     <div>
       <span>{task.id}</span>
       {' : '}
-      <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">{task.title}</span>
+      <Link href={`/tasks/${task.id}`}>
+        <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">{task.title}</span>
+      </Link>
     </div>
   )
 }
